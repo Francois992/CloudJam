8adfa6453a9c6315665fde100f2e2cef29ab8291
Eloi FG <eloi-fg@gmx.com>
2020-11-03 18:25:32 +0100

    added menu sounds

---
+++ Assets/Scripts/MusicScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour
+{
+
+    public AudioSource source;
+    public AudioClip menuMusic;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (!source.isPlaying)
+        {
+            source.PlayOneShot(menuMusic);
+            source.loop = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
