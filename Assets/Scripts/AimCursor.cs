a6d03fffefd52538d90f02a5cc9b9b1a8f7946fc
Yekonori <yannisdemacedo@gmail.com>
2020-11-04 10:33:37 +0100

    AimCursor

---
+++ Assets/Scripts/AimCursor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimCursor : MonoBehaviour
+{
+    #region Script Parameters
+
+    [SerializeField]
+    private Player player;
+
+    [SerializeField]
+    private float aimSpeed = 1.5f;
+
+    #endregion
+
+    #region Fields
+
+    private float xPos;
+    private float yPos;
+
+    private Rewired.Player playerController;
+
+    #endregion
+
+    #region Unity Methods
+
+    private void FixedUpdate()
+    {
+        xPos = playerController.GetAxis("MoveHorizontal");
+        yPos = playerController.GetAxis("MoveVertical");
+
+        transform.Translate(new Vector2(xPos, yPos) * aimSpeed * Time.fixedDeltaTime);
+    }
+
+    #endregion
+
+    public void SetPlayer(Player playerToSet)
+    {
+        player = playerToSet;
+        playerController = player.playerController;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,7 +11,7 @@ public class Player : MonoBehaviour
     public int bets = 1;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         playerController = ReInput.players.GetPlayer(playerId);
     }
