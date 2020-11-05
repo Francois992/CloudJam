05ec5f65ae790089eeb5fc1411f76e3db8f5d143
Yekonori <yannisdemacedo@gmail.com>
2020-11-05 09:32:04 +0100

    SecondBreath stats base

--- Assets/Scripts/Octopus.cs
+++ Assets/Scripts/Octopus.cs
@@ -29,11 +29,57 @@ public class Octopus : MonoBehaviour
     [SerializeField]
     private bool isAlreadyStun = false;
 
+    [Header("Second Souffle")]
+
+    [SerializeField]
+    private float secondBreathSpeedBonus = 1.25f;
+
+    [SerializeField]
+    private float secondBreathDuration = 1f;
+
+    [SerializeField]
+    private float secondBreathBaseChance = 1f;
+
+    [SerializeField]
+    private List<float> secondBreathPositionBonus = new List<float>() { 0f, 3f, 5f, 8f };
+
+    [Header("Second Souffle - Rage")]
+
+    [SerializeField]
+    private float secondBreathRage = 0f;
+
+    [SerializeField]
+    private float secondBreathRageMax = 3f;
+
+    [SerializeField]
+    private float secondBreathRageDecrease = 0.5f;
+
+    [SerializeField]
+    private float secondBreathRageDecreaseDelay = 1f;
+
+    [Header("Second Souffle - Tension")]
+
+    [SerializeField]
+    private float secondBreathTension = 0f;
+
+    [SerializeField]
+    private float secondBreathTensionMax = 3f;
+
+    [SerializeField]
+    private float secondBreathTensionDecrease = 1f;
+
+    [SerializeField]
+    private float secondBreathTensionDecreaseDelay = 1f;
+
     #endregion
 
     #region Fields
 
     private float originalSpeed;
+
+    private bool canBreathAgain = true;
+
+    private float currentRageTimer = 0;
 
     #endregion
 
@@ -125,6 +171,23 @@ public class Octopus : MonoBehaviour
     private void ResetInvinsible()
     {
         isInvinsible = false;
+    }
+
+    #endregion
+
+    #region Second Souffle
+
+    private void SecondBreath()
+    {
+        //Besoin de mettre l'index de la position dans la course - 1 !
+        float secondBreathChance = secondBreathBaseChance + secondBreathPositionBonus[0] + secondBreathRage + secondBreathTension;
+
+        int rnd = Random.Range(0, 101);
+
+        if (rnd <= secondBreathBaseChance)
+        {
+            octopusSpeed *= secondBreathSpeedBonus;
+        }
     }
 
     #endregion
